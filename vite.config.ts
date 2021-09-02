889f97ee6353c12ecca5a99c9a902f379a3c897a
Jecelyn <jec@google.com>
2021-09-02 15:57:17 +0800

    Update read me

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    sourcemap: true,
+  }
 })
