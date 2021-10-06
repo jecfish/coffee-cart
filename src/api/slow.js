657558cf2921ff6a16ffaa8edf4e2a6070759ae6
Jecelyn <jec@google.com>
2021-10-06 12:11:34 +0800

    almost invisible

--- src/api/slow.js
+++ src/api/slow.js
@@ -7,7 +7,7 @@ export async function makeItSlow() {
   const useless = await import('./bigbigcode2.js');
   console.log(useless);
 
-  `rgba(${getRandomArbitrary(0, 255)}, ${getRandomArbitrary(0, 255)}, ${getRandomArbitrary(0, 255)}, 0.1)`;
+  `rgba(${getRandomArbitrary(0, 255)}, ${getRandomArbitrary(0, 255)}, ${getRandomArbitrary(0, 255)}, 0.002)`;
   for (let i = 0; i < 10000; i++) {
     let random = 0;
     random = Math.floor(Math.random() * 100);
