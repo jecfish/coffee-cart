d9eeb285e1e7bcb704ae32b2c4cf9ca3b80d7d4c
Jecelyn Yeen <jec@google.com>
2022-08-21 13:23:35 +0300

    Change fetch to xhr

--- src/api/coffee.api.ts
+++ src/api/coffee.api.ts
@@ -6,4 +6,23 @@ export default {
     // const url = ''
     return fetch(url).then(res => res.json()).then(x => wait(waitTime, x));
   },
+  getListXhr(waitTime = 0) {
+    const url = '/list.json'
+
+    const promisetify = new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+      xhr.onload = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          const list = JSON.parse(this.responseText);
+          resolve(list);
+        } else {
+          reject({status: this.status, statusText: this.statusText})
+        }
+      }
+      xhr.send();
+    });
+
+    return promisetify.then(x => wait(waitTime, x));
+  },
 }
