8ef4726069412279d4cea986f7d378f688d92ef8
Mr. Yap <15719191+mryap@users.noreply.github.com>
2022-09-09 10:10:49 +0800

    Update GitHubPage.vue

--- src/components/pages/GitHubPage.vue
+++ src/components/pages/GitHubPage.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
-    <p>Star our repository <a href="https://github.com/jecfish/coffee-cart">jecfish/coffee-cart</a>. Report in the repository if you found any issues.</p>
-    <p>Here are the extra actions you can perform apart from the <a href="https://goo.gle/devtools-recorder">usual add to cart flows.</a></p>
+   
     <ul>
       <li>Double click on coffee title to translate it to Chinese.</li>
       <li>Right click on coffee icon to open an add to cart dialog.</li>
@@ -10,11 +9,7 @@
       <li>The add to cart process will be slowing down (intentionally) when the cart has more than 7 items.</li>
       <li>Slow down page load performance with ads by passing in an <code>ads</code> param. <a href="https://coffee-cart.netlify.app/?ad=1">https://coffee-cart.netlify.app/?ad=1</a></li>
     </ul>
-    <p>You can use this demo to learn DevTools features. We have tutorial and video to guide you.</p>
-    <ul>
-      <li>Record, replay and measure user flows with the Recorder panel (<a href="https://goo.gle/devtools-recorder">link</a>).</li>
-      <li>Get actionable insights for your websites' performance with the Performance insights panel (<a href="https://goo.gle/devtools-performance">link</a>) </li>
-    </ul>
+
   </div>
 </template>
 
