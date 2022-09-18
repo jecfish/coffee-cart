efb44c86419d1762523b4795711bfbb378b589e2
Mr. Yap <15719191+mryap@users.noreply.github.com>
2022-09-18 12:04:55 +0800

    Update GitHubPage.vue
    try to bold the text with markdown

--- src/components/pages/GitHubPage.vue
+++ src/components/pages/GitHubPage.vue
@@ -2,7 +2,7 @@
   <div class="container">
    
     <ul>
-      <li>Double click on coffee title to translate it to Chinese.</li>
+      <li>** Double click ** on coffee title to translate it to Chinese.</li>
       <li>Right click on coffee icon to open an add to cart dialog.</li>
       <li>[Desktop only] Hover over Pay button will show a quick cart preview, click to add or remove items.</li>
       <li>A random promo coffee pop up show up when adding every 3rd items to the cart. (e.g. 3, 6, 9, ...)</li>
