0e47383aa321ca2cdb26c28bd8927dba221251a4
Jecelyn Yeen <jec@google.com>
2024-03-11 10:24:10 +0100

    Updaete url

--- src/api/coffee.api.ts
+++ src/api/coffee.api.ts
@@ -2,7 +2,7 @@ import { wait } from '../utils';
 
 export default {
   getList(waitTime = 0) {
-    const url = '/coffee-list.json'
+    const url = '/list.json'
     // const url = ''
     return fetch(url).then(res => res.json()).then(x => wait(waitTime, x));
   },
